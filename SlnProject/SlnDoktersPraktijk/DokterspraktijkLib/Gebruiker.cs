4ca0a2fab1b8f5629e504ff02920ef9b40b3d3bc
daviduser-max <david.sarwarghauri@student.odisee.be>
2026-05-14 16:54:23 +0200

    Eerste push

---
+++ SlnProject/SlnDoktersPraktijk/DokterspraktijkLib/Gebruiker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DokterspraktijkLib
+{
+    internal class Gebruiker
+    {
+        public int Id { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+
+        public string Email {  get; set; }
+
+        public string Passwoord { get; set; }
+
+        public string Gsm {  get; set; }
+
+        public Byte[] Profielfotodata {  get; set; }
+
+        public string VolledigeNaam => $"{Voornaam} {Achternaam}";
+
+
+
+    }
+}
